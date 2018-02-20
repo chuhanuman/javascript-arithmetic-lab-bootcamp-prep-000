9d8f15e21bf9c3447fee6145745c2ebf69d45773
Hanuman Chu <chuhanuman@gmail.com>
2018-02-20 11:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ function divide(a,b){
   return a / b
 }
 function inc(n){
-  return n++
+  n+=1
+  return n
 }
 function dec(n){
-  return n--
+  n-=1
+  return n
 }
 function makeInt(n){
   return parseInt(n, 10)
